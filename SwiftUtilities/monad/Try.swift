a48f729e82dd64c9ecac8fee99bc219d001ec6c8
Hai Pham <viethai.pham@holmusk.com>
2017-07-31 10:40:47 +0800

    Added method to convert Optional to Try

--- SwiftUtilities/monad/Try.swift
+++ SwiftUtilities/monad/Try.swift
@@ -138,3 +138,43 @@ extension Try: TryType {
         }
     }
 }
+
+extension Try {
+    
+    /// Produce a Try from an Optional, and throw an Error if the value is
+    /// absent.
+    ///
+    /// - Parameters:
+    ///   - optional: An Optional instance.
+    ///   - error: The error to be thrown when there is no value.
+    /// - Returns: A Try instance.
+    public static func from<Val>(optional: Optional<Val>, error: Error) -> Try<Val> {
+        switch optional {
+        case .some(let value):
+            return Try<Val>.success(value)
+            
+        case .none:
+            return Try<Val>.failure(error)
+        }
+    }
+    
+    /// Produce a Try from an Optional, and throw an Error if the value is
+    /// absent.
+    ///
+    /// - Parameters:
+    ///   - optional: An Optional instance.
+    ///   - error: The error to be thrown when there is no value.
+    /// - Returns: A Try instance.
+    public static func from<Val>(optional: Optional<Val>, error: String) -> Try<Val> {
+        return Try.from(optional: optional, error: Exception(error))
+    }
+    
+    /// Produce a Try from an Optional, and throw a default Error if the value is
+    /// absent.
+    ///
+    /// - Parameter optional: An Optional instance.
+    /// - Returns: A Try instance.
+    public static func from<Val>(optional: Optional<Val>) -> Try<Val> {
+        return Try.from(optional: optional, error: "Cannot be nil")
+    }
+}
